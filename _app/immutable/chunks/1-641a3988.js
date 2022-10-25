033d1946d62b45039f1570b69a8c80d1678ff2ad
Genji Fujimori <undefined>
2022-10-25 15:42:02 +0900

    Updates

---
+++ _app/immutable/chunks/1-641a3988.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-c399d6aa.js";export{t as component};
---
+++ _app/immutable/chunks/2-338eef3b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-40bd0bbe.js";export{t as component};
