b7e16e50e6fea1f983bddcab589cc38659c61433
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-10-25 16:40:19 +0900

    DOES NOT WORK

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,8 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: process.env.GITHUB_REPO,
+  repo: 'https://github.com/kintone-workshops/intro-to-svelte.git', // Update to point to your repository
+  // repo: process.env.GITHUB_REPO,
   user: {
    name: process.env.GITHUB_NAME,
    email: process.env.GITHUB_EMAIL
