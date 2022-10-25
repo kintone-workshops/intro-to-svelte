3ae0e2f0b19b929d0f0575807b15c27507a02bcf
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-10-25 15:40:50 +0900

    debug for static

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,10 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: '200.html'
 		})
 	}
 };
